3ea479b2b2848240833c7997981fb703cc1f5a8b
gio_sxis <92173444+giosx@users.noreply.github.com>
2024-02-07 12:43:21 -0300

    Add files via upload

---
+++ hehe/hehe.js
@@ -0,0 +1,33 @@
+// Variável da pergunta analisada
+const question = "Qual time você torce?"
+
+// Verifica se é pergunta
+
+if(question.endsWith("?"));
+
+question.indexOf("a")
+
+//toUpperCase, toLower
+
+console.log(question.toUpperCase())
+console.log(question.toLowerCase())
+
+// Split
+console.log(question.split(" "));
+
+//Procurar os sujeitos e mudar para eu
+//Procurar verbos e mudar para 1a pessoa
+
+// Supõe o que é a pergunta
+
+
+// Trata a resposta
+
+
+// Monta a resposta
+
+
+// mostra no terminal
+
+console.log(question)
+
